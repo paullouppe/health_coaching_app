daf456ec9aca8c28e2081ab8631e5f1f8a1d33c1
noelliemartin <99398397+noelliemartin@users.noreply.github.com>
2024-02-20 10:36:02 +0100

    add search bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,10 +8,10 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-  } from "@/components/ui/card"
+} from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
-
-import { Skeleton } from "@/components/ui/skeleton"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 function Home() {
 
@@ -36,28 +36,37 @@ function Home() {
         return (<div>Errors</div>)
     }
 
-    if(isLoading) {
+    if (isLoading) {
         return (<div>loading ...</div>)
     }
 
     return (
         <>
             <div className="container mx-auto px-4">
-                <h1 className="text-center text-4xl mt-6 mb-4">Health App</h1>
+                <header className="flex items-centerd mt-10 mb-10">
+                    <img className="w-10 h-10" src="./public/logo_app.png"></img>
+                    <h1 className="text-center text-4xl ml-3">Body Boost</h1>
+                </header>
+                <div className="flex w-full max-w-sm items-center space-x-2 mb-5">
+                    <Input type="search" placeholder="Search..." />
+                    <Button type="submit">
+                        <img src="src/assets/search_logo.png"></img>
+                    </Button>
+                </div>
                 <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
-                    {patients.map((p,index) => (
+                    {patients.map((p, index) => (
                         <div key={index}>
-                        <Link to={"/patient/"+p.id}>
-                            <Card className="flex items-center transition-transform active:scale-95">
-                                <img className="ml-4 w-10 h-10" src={p.icon}></img>
-                                <CardHeader>
-                                  <CardTitle><span className="uppercase">{p.lastname}</span> {p.firstname}</CardTitle>
-                                  <CardDescription>
-                                    <Badge className="capitalize text-neutral-500	" variant="secondary">{p.activityProfile}</Badge>
-                                  </CardDescription>
-                                </CardHeader>
-                            </Card>
-                          </Link>
+                            <Link to={"/patient/" + p.id}>
+                                <Card className="flex items-center transition-transform active:scale-95">
+                                    <img className="ml-4 w-10 h-10" src={p.icon}></img>
+                                    <CardHeader>
+                                        <CardTitle><span className="uppercase">{p.lastname}</span> {p.firstname}</CardTitle>
+                                        <CardDescription>
+                                            <Badge className="capitalize text-neutral-500	" variant="secondary">{p.activityProfile}</Badge>
+                                        </CardDescription>
+                                    </CardHeader>
+                                </Card>
+                            </Link>
                         </div>
                     ))}
                 </div>
