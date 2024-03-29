3e6253508eff5dcf5a5fcec9ebaa1c3718373117
Rusty <paullouppe@gmail.com>
2024-03-29 13:16:24 +0100

    basic sign in

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,44 @@
+import { useForm } from 'react-hook-form';
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 
 function Login({ setToken }) {
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
-  return (
-    <h2 onClick={() => setToken("aatooookkkkkeeeen")}>Login</h2>
-  )
+    const onSubmit = data => {
+      setToken("token")
+      console.log(data);
+    };
+
+    return (
+      <div className="container mx-auto px-4">
+        <form onSubmit={handleSubmit(onSubmit)} className="mt-10 flex flex-col gap-4">
+          <h1 className="font-bold text-2xl">Login</h1>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <Input
+              id="email"
+              type="email"
+              {...register("email", { required: "Email is required", pattern: /^\S+@\S+$/i })}
+            />
+            {errors.email && <p>{errors.email.message}</p>}
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <Input
+              id="password"
+              type="password"
+              {...register("password", { required: "Password is required" })}
+            />
+            {errors.password && <p>{errors.password.message}</p>}
+          </div>
+          <Button type="submit">Login</Button>
+
+          <a className="text-sm" href="#">I don't have an account</a>
+        </form>
+      </div>
+    );
 }
 
 export default Login
