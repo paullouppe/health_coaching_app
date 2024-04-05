a4f02608f1ee824246375066df729dfc4a158dc3
paullouppe <paullouppe@gmail.com>
2024-04-05 10:35:34 +0200

    <fix> button hover color

--- src/pages/functional_pages/Errors.jsx
+++ src/pages/functional_pages/Errors.jsx
@@ -3,7 +3,7 @@ function Errors(props) {
     return (
         <div className="text-center">
             <img src="" alt="img d'erreur"/>
-            <h1 className="text-xl">Error batar</h1>
+            <h1 className="text-xl">Error</h1>
         </div>
     )
 }
